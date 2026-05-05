55495a0b48aa5edb952a53d338cfb3ad6683ed50
Nouran Atef <engineer_nouran@yahoo.com>
2026-05-05 14:36:37 +0200

    initialize project structure with Gradle, Ktor, and Docker support; add health check endpoint test

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    id("io.ktor.plugin") version "2.3.5"
+}
+
+group = "com.nouran"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.5")
+    implementation("io.ktor:ktor-server-netty:2.3.5")
+
+    testImplementation("io.ktor:ktor-server-test-host:2.3.5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.20")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.nouran.ApplicationKt"
+    }
+
+    val dependencies = configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    }
+
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.test {
+    useJUnit()
+}
---
+++ src/test/kotlin/com/nouran/ApplicationTest.kt
@@ -0,0 +1,24 @@
+package com.nouran
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import java.lang.classfile.Attributes.module
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun `health endpoint should return UP`() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/health")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("UP", response.bodyAsText())
+    }
+}
